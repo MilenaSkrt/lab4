63eaa3a48aedf0bafabd2fadcecf27e904cf5537
MilenaSkrt <135116799+MilenaSkrt@users.noreply.github.com>
2024-05-15 09:11:19 +0500

    Update lk.cpp

--- lk.cpp
+++ lk.cpp
@@ -2,3 +2,12 @@
         for (int i = 0; i < count * 2; i += 2) {
             Insert(buf[i], i + 1);
         }
+
+ void DuplicateAll() override {
+        Node* current = head;
+        int index = 0;
+        while (current != nullptr) {
+            Insert(current->Data, index + 1);
+            current = current->Next;
+            index += 2;
+        }
